6ba5e399cab33156f171028e04ccfe147c72945a
THammarback <62379595+THammarback@users.noreply.github.com>
2024-12-03 07:49:18 +0100

    Create tools.ts

---
+++ tools.ts
@@ -0,0 +1,41 @@
+
+type LessThan<
+  A extends number,
+  B extends number,
+  S extends any[] = []
+> = S["length"] extends B
+  ? false
+  : S["length"] extends A
+  ? true
+  : LessThan<A, B, [...S, any]>;
+type Subtract<
+  A extends number,
+  B extends number,
+  I extends any[] = [],
+  O extends any[] = []
+> = LessThan<A, B> extends true
+  ? never
+  : LessThan<I["length"], A> extends true
+  ? Subtract<
+      A,
+      B,
+      [...I, any],
+      LessThan<I["length"], B> extends true ? O : [...O, any]
+      >
+    : O["length"];
+
+
+type NestedArray<T, Depth extends number> = Depth extends 0 ? T : NestedArray<T[], Subtract<Depth, 1>>;
+type Length<T extends any[]> = T['length'];
+
+export function preParse<T,N extends string[]>(input:string, mapping:(x:string)=>T, ...separators: N): NestedArray<T, Length<N>>{
+  const splitRecursively = (str: string, seps: string[]): any => {
+    if (seps.length === 0) {
+      return mapping(str);
+    }
+    const [firstSep, ...restSeps] = seps;
+    return str.split(firstSep).filter(x=>x).map(subStr => splitRecursively(subStr, restSeps));
+  };
+
+  return splitRecursively(input, separators);
+}
